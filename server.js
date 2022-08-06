b5158b041815b2ba507b0b28c78b4ea6e549cba7
Anthony Salemo <ajssalemo@gmail.com>
2022-08-06 15:30:03 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// IMPORTANT - This is needed - see this: https://yarnpkg.com/features/pnp#initializing-pnp
+// Or else this will fail since iisnode is running directly against our entrypoint (server.js) and NOT starting the application with package.json scripts (yarn start)
+require('./.pnp.cjs').setup();
 const express = require("express");
 
 const app = express();
