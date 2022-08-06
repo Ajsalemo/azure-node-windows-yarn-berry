7842c6eec593a80aafa8da71e1c0ac3407ae1360
Anthony Salemo <ajssalemo@gmail.com>
2022-08-06 15:38:56 -0400

    initial commit

--- deploy.cmd
+++ deploy.cmd
@@ -92,19 +92,30 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   IF !ERRORLEVEL! NEQ 0 goto error
 )
 
-  echo "Installing corepack.."
-  call :ExecuteCmd npm i -g corepack
-  echo "Setting yarn to path.."
-  SET PATH=%PATH%;D:\local\AppData\npm
-  
+echo "Installing corepack.."
+call :ExecuteCmd npm i -g corepack
+echo "Setting yarn to path.."
+SET PATH=%PATH%;D:\local\AppData\npm
+
+:: IF USING 'Zero Installs'
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" IF EXIST "%DEPLOYMENT_TARGET%\.yarn\cache\" (
+  pushd "%DEPLOYMENT_TARGET%"  
+  echo ".yarn/cache checked in for 'Zero Installs, pushing cache and not running install.."
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
+)
+
+:: If NOT using 'Zero-Installs'
 :: Technically, setting up a project with yarn 2 is using Zero-Installs by default
-:: If .yarn/cache is checked in and pushed, then we technically shouldn't even need to run the below - see this: https://yarnpkg.com/getting-started/install#initializing-your-project
+:: If .yarn/cache is checked in to source and pushed, then we shouldn't even need to run the below - see this: https://yarnpkg.com/getting-started/install#initializing-your-project
 :: However, if the project is NOT set to use 'Zero-Installs', the below can be used to change the yarn version to stable
-:: 3. Install npm packages
-IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+:: 3. Install yarn packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" IF NOT EXIST "%DEPLOYMENT_TARGET%\.yarn\cache\" (
   pushd "%DEPLOYMENT_TARGET%"  
   echo "Setting yarn version to stable.."
   call :ExecuteCmd yarn set version stable
+  echo "Checking yarn version.."
+  call :ExecuteCmd yarn -v
   echo "Running yarn install.."
   call :ExecuteCmd yarn install 
   IF !ERRORLEVEL! NEQ 0 goto error
